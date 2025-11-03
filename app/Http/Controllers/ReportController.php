3dc562efa67da43ee5e4155f67fb0e78f3ac33ec
mahdi paykoub <mahdi.paykoub.github@gmail.com>
2025-11-03 11:44:42 -0800

    add default user image to pdf file

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,24 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Grade;
+use App\Models\Major;
 use App\Models\Student;
 use Illuminate\Support\Facades\Storage;
 use niklasravnsborg\LaravelPdf\Facades\Pdf;
+use Illuminate\Http\Request;
 
 class ReportController extends Controller
 {
     public function seatNumberView()
     {
+        $grades = Grade::all();
+        $majors = Major::all();
         $students = Student::with(['grade', 'major', 'school', 'products'])
             ->whereNotNull('seat_number')
             ->get();
-        return view('reports.seats.index', compact('students'));
+        return view('reports.seats.index', compact('students' ,'grades' , 'majors'));
     }
 
-    public function generatePdf()
+    public function generatePdf(Request $request)
     {
-        $students = Student::with(['grade', 'major'])->whereNotNull('seat_number')->get();
-        
+        // گرفتن فیلترها از request
+        $gender = $request->input('gender');       // male/female یا null
+        $grade_id = $request->input('grade_id');   // عدد یا null
+        $major_id = $request->input('major_id');   // عدد یا null
+
+        // query اصلی
+        $students = Student::with(['grade', 'major'])
+            ->whereNotNull('seat_number')
+            ->when($gender, function ($query, $gender) {
+                $query->where('gender', $gender);
+            })
+            ->when($grade_id, function ($query, $grade_id) {
+                $query->where('grade_id', $grade_id);
+            })
+            ->when($major_id, function ($query, $major_id) {
+                $query->where('major_id', $major_id);
+            })
+            ->get();
+
         foreach ($students as $student) {
             if ($student->photo) {
                 $realPath = storage_path('app/private/students/' . basename($student->photo));
@@ -27,10 +49,10 @@ class ReportController extends Controller
                 if (file_exists($realPath)) {
                     $student->photo_path = 'file:///' . str_replace('\\', '/', $realPath);
                 } else {
-                    $student->photo_path = null;
+                    $student->photo_path = public_path('download.jpg');
                 }
             } else {
-                $student->photo_path = null;
+                $student->photo_path = public_path('download.jpg');
             }
         }
 
--- resources/views/reports/seats/index.blade.php
+++ resources/views/reports/seats/index.blade.php
@@ -16,21 +16,57 @@
     <div class="alert alert-danger">{{ session('error') }}</div>
     @endif
 
-    <div class="row">
-        <div class="text-start w-100">
-            <a href="{{ route('students.pdf.generate') }}" class="btn btn-success bg-admin-green me-auto mb-3">
-                چاپ شماره صندلی
-            </a>
-        </div>
-    </div>
-    <div class="table-wrap table-responsive-xl students-list-table">
 
+    <div class="table-wrap table-responsive-xl students-list-table">
+        <form action="{{ route('students.pdf.generate') }}" method="GET" class="mb-3">
+            @csrf
+
+            <div class="row align-items-center">
+                <div class="mb-3 col-lg-3">
+                    <label for="gender">جنسیت</label>
+                    <select name="gender" id="gender" class="form-control">
+                        <option value="">همه</option>
+                        <option value="male" {{ request('gender') == 'male' ? 'selected' : '' }}>مرد</option>
+                        <option value="female" {{ request('gender') == 'female' ? 'selected' : '' }}>زن</option>
+                    </select>
+                </div>
+
+                <div class="mb-3 col-lg-3">
+                    <label for="grade_id">پایه</label>
+                    <select name="grade_id" id="grade_id" class="form-control">
+                        <option value="">همه</option>
+                        @foreach($grades as $grade)
+                        <option value="{{ $grade->id }}" {{ request('grade_id') == $grade->id ? 'selected' : '' }}>
+                            {{ $grade->name }}
+                        </option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <div class="mb-3 col-lg-3">
+                    <label for="major_id">رشته</label>
+                    <select name="major_id" id="major_id" class="form-control">
+                        <option value="">همه</option>
+                        @foreach($majors as $major)
+                        <option value="{{ $major->id }}" {{ request('major_id') == $major->id ? 'selected' : '' }}>
+                            {{ $major->name }}
+                        </option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="mb-3 col-lg-3 mt-4">
+                    <button type="submit" class="btn btn-success bg-admin-green">چاپ شماره صندلی</button>
+                </div>
+            </div>
+
+        </form>
         <table class="table table-striped align-middle">
             <thead class="table-light">
                 <tr>
                     <th>عکس</th>
                     <th>نام</th>
                     <th>نام خانوادگی</th>
+                    <th>جنسیت</th>
                     <th>پایه</th>
                     <th>رشته</th>
                     <th>شماره صندلی</th>
@@ -51,6 +87,13 @@
 
                     <td>{{ $student->first_name }}</td>
                     <td>{{ $student->last_name }}</td>
+                    <td>
+                        @if($student->gender == 'male')
+                        <span>پسر</span>
+                        @else
+                        <span>دختر</span>
+                        @endif
+                    </td>
                     <td>{{ optional($student->grade)->name }}</td>
                     <td>{{ optional($student->major)->name }}</td>
                     <td>{{ $student->seat_number }}</td>
